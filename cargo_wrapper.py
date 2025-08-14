7d9da92e20d3e54e5bdd7cad1ca2e05a75321027
David Peled <davidp.info1@gmail.com>
2025-08-14 23:30:03 +0300

    feat(toml): add toml formatter and linter

---
+++ cargo_wrapper.py
@@ -0,0 +1,40 @@
+import logging
+import shutil
+import subprocess
+import sys
+from pathlib import Path
+
+logger = logging.getLogger(__name__)
+
+
+def exit_if_no_cargo() -> Path:
+    cargo = shutil.which("cargo")
+    if not cargo:
+        logger.error(
+            "cargo not installed. Please install Rust from https://rustup.rs/!"
+        )
+        sys.exit(1)
+
+    return Path(cargo).resolve()
+
+
+def install_taplo(cargo: Path) -> None:
+    if not shutil.which("taplo"):
+        # ruff: noqa: S603 (Allow passing cargo command as parameter).
+        result = subprocess.run(
+            [cargo, "install", "taplo-cli"],
+            check=False,  # let us inspect returncode instead of raising
+        )
+
+        if result.returncode == 0:
+            logger.info("✅ Taplo installed successfully.")
+        else:
+            logger.error(
+                "❌ Failed to execute `cargo install taplo-cli`, please debug. "
+            )
+            sys.exit(result.returncode)
+
+
+if __name__ == "__main__":
+    cargo_path = exit_if_no_cargo()
+    install_taplo(cargo_path)
