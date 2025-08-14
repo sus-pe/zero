ff10fcb715afe3192d3cee985a3d2362f6a6a8ca
David Peled <davidp.info1@gmail.com>
2025-08-14 21:30:55 +0300

    refactor(tests): move game fixture to conftest

--- tests/acceptance/conftest.py
+++ tests/acceptance/conftest.py
@@ -1,9 +1,19 @@
+from collections.abc import AsyncGenerator
 from os import environ
+from typing import Any
 
-from pytest import fixture
+from pytest_asyncio import fixture
+
+from zero.game import Game
 
 
 @fixture(scope="session", autouse=True)
-def sdl_headless_env() -> None:
+async def sdl_headless_env() -> None:
     # Must be set before pygame.init()
     environ.setdefault("SDL_VIDEODRIVER", "dummy")
+
+
+@fixture
+async def game() -> AsyncGenerator[Game, Any]:
+    async with Game() as game:
+        yield game
