2191386e160fcc5ec7dc6ba7ccf3f511eba00225
David Peled <davidp.info1@gmail.com>
2025-08-25 22:06:14 +0300

    refactor(tests): mimic project structure in tests
    also some acceptance/integration moved to unit

--- tests/unit/zero/game_test.py
+++ tests/unit/zero/game_test.py
@@ -4,6 +4,29 @@ from zero.game import Game
 from zero.mouse import MouseCursorEvent
 
 
+async def test_game_fullscreen(game: Game) -> None:
+    game.set_windowed()
+    assert game.is_windowed()
+    game.set_fullscreen()
+    assert game.is_fullscreen()
+    game.set_fullscreen()
+    assert game.is_fullscreen()
+    game.toggle_fullscreen()
+    assert game.is_windowed()
+    game.toggle_fullscreen()
+    assert game.is_fullscreen()
+
+
+async def test_game_fullscreen_button(game: Game) -> None:
+    assert game.is_windowed()
+    game.send_f11()
+    await game.wait_next_loop()
+    assert game.is_fullscreen()
+    game.send_f11()
+    await game.wait_next_loop()
+    assert game.is_windowed()
+
+
 async def test_mouse_cursor(
     game: Game, stub_mouse_events: Iterable[MouseCursorEvent]
 ) -> None:
