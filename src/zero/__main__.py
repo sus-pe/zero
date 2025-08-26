1652bd6e09a763a61f6e04fb9dc640cc6f6a6779
David Peled <davidp.info1@gmail.com>
2025-08-26 18:59:42 +0300

    refactor(tests): ensure no errors
    This fix "no fast rendered errors"

--- src/zero/__main__.py
+++ src/zero/__main__.py
@@ -5,16 +5,30 @@ import typer
 from zero.game_loader import GameLoader
 
 
-async def async_main(*, send_quit: bool = False) -> int:
-    async with GameLoader() as game:
+async def async_main(
+    *,
+    resizeable: bool,
+    scaled: bool,
+    send_quit: bool = False,
+) -> int:
+    async with GameLoader(resizeable=resizeable, scaled=scaled) as game:
         if send_quit:
             await game.send_quit()
         await game.wait_exit()
     return 0
 
 
-def main(*, send_quit: bool = False) -> int:  # pragma: no cover
-    return asyncio.run(async_main(send_quit=send_quit))
+def main(
+    *, send_quit: bool = False, resizeable: bool = True, scaled: bool = True
+) -> int:
+    # pragma: no cover
+    return asyncio.run(
+        async_main(
+            send_quit=send_quit,
+            resizeable=resizeable,
+            scaled=scaled,
+        )
+    )
 
 
 if __name__ == "__main__":  # pragma: no cover
