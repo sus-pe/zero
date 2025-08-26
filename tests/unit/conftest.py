f61d1e6945d91c20221a6879da860b28f9c4cbcd
David Peled <davidp.info1@gmail.com>
2025-08-26 08:43:16 +0300

    refactor(game): introduce the game loader

--- tests/unit/conftest.py
+++ tests/unit/conftest.py
@@ -4,9 +4,10 @@ from typing import Any
 from pytest_asyncio import fixture
 
 from zero.game import Game
+from zero.game_loader import GameLoader
 
 
 @fixture
 async def game() -> AsyncGenerator[Game, Any]:
-    async with Game() as game:
+    async with GameLoader() as game:
         yield game
