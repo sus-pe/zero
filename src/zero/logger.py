cc3087c55182213447fe38f4b4faa34667cf2886
David Peled <davidp.info1@gmail.com>
2025-09-06 13:59:49 +0300

    feat(logger): support logger

--- src/zero/logger.py
+++ src/zero/logger.py
@@ -1,10 +1,10 @@
 from logging import Logger as _Logger
 from logging import getLogger
-from typing import Protocol
+from typing import Any, Protocol
 
 
 class Logger(Protocol):
-    pass
+    def critical(self, msg: str, *args: Any, **kwargs: Any) -> None: ...  # noqa: ANN401
 
 
 def create_for(name: str) -> _Logger:
